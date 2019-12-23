8fc4cb267933c3d981077b26f2221c045d92a3d4
Mochamad Iqbal Dwi Cahyo <mochadwi@users.noreply.github.com>
2019-12-24 00:19:42 +0700

    Update merge-request.sh (#2)
    - use regex instead of `pj`
    - enable `squash` commits by default

--- merge-request.sh
+++ merge-request.sh
@@ -10,22 +10,26 @@ fi
 # Extract the host where the server is running, and add the URL to the APIs
 [[ $CI_PROJECT_URL =~ ^https?://[^/]+ ]] && HOST="${BASH_REMATCH[0]}/api/v4/projects/"
 
+if [ -z "$HOST" ]; then
+  echo "Cannot connect to empty $HOST"
+  exit 1
+fi
+
 # Look which is the default branch
-TARGET_BRANCH=`curl --silent "${HOST}${CI_PROJECT_ID}" --header "PRIVATE-TOKEN:${GITLAB_PRIVATE_TOKEN}" | jq --raw-output '.default_branch'`;
+TARGET_DEFAULT_BRANCH=`curl --silent "${HOST}${CI_PROJECT_ID}" --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}" | grep -Po '"default_branch":.*?\[^\\]",'`;
+TARGET_BRANCH="dev"
 
 # The description of our new MR, we want to remove the branch after the MR has
 # been closed
-# \"message\": \"${CI_COMMIT_MESSAGE}\",
 BODY="{
     \"id\": ${CI_PROJECT_ID},
     \"source_branch\": \"${CI_COMMIT_REF_NAME}\",
     \"target_branch\": \"${TARGET_BRANCH}\",
     \"remove_source_branch\": true,
+    \"squash\": true,
     \"title\": \"WIP: ${CI_COMMIT_REF_NAME}\",
     \"assignee_id\":\"${GITLAB_USER_ID}\"
 }";
-
-echo "message commit: ${CI_COMMIT_MESSAGE}"
 
 # Require a list of all the merge request and take a look if there is already
 # one with the same source branch
@@ -34,19 +38,10 @@ COUNTBRANCHES=`echo ${LISTMR} | grep -o "\"source_branch\":\"${CI_COMMIT_REF_NAM
 
 # No MR found, let's create a new one
 if [ ${COUNTBRANCHES} -eq "0" ]; then
-    echo "Host: ${HOST}"
-    echo "Body: ${BODY}"
-    curl -v -X POST "${HOST}${CI_PROJECT_ID}/merge_requests" \
+    curl -X POST "${HOST}${CI_PROJECT_ID}/merge_requests" \
         --header "PRIVATE-TOKEN:${GITLAB_PRIVATE_TOKEN}" \
         --header "Content-Type: application/json" \
-        --data '{
-    "id": ${CI_PROJECT_ID},
-    "source_branch": "${CI_COMMIT_REF_NAME}",
-    "target_branch": "${TARGET_BRANCH}",
-    "remove_source_branch": true,
-    "title": "WIP: ${CI_COMMIT_REF_NAME}",
-    "assignee_id":"${GITLAB_USER_ID}"
-}';
+        --data "${BODY}";
 
     echo "Opened a new merge request: WIP: ${CI_COMMIT_REF_NAME} and assigned to you";
     exit;
